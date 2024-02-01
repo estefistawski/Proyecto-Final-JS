944d8b9902e3e0067afba01afa1d63e46e96ada8
estefistawski <estefaniapazstawski@hotmail.com>
2024-01-31 23:35:30 -0300

    Listas completas con objetos

--- index.js
+++ index.js
@@ -2,17 +2,76 @@ const listaPersonajes = [];
 const listaLibros = [];
 const listaCasas = [];
 const listaHechizos = [];
+const favoritos = [];
+let imgCero = 0;
+class Personaje {
+    constructor(id, nombre, casa, cumpleaños, interprete, img) {
+        this.id = id;
+        this.nombre = nombre;
+        this.casa = casa;
+        this.cumpleaños = cumpleaños;
+        this.interprete = interprete;
+        this.img = img;
+    }
+}
+class Libro {
+    constructor(id, titulo, fecha, paginas, portada) {
+        this.id = id;
+        this.titulo = titulo;
+        this.fecha = fecha;
+        this.paginas = paginas;
+        this.portada = portada;
+    }
+}
+class Casa{
+    constructor(id,nombre,emoji,fundador,animal){
+        this.id=id;
+        this.nombre=nombre;
+        this.emoji=emoji;
+        this.fundador=fundador;
+        this.animal=animal;
+    }
+}
+class Hechizo{
+    constructor(id,nombre,uso){
+        this.id=id;
+        this.nombre=nombre;
+        this.uso=uso;
+    }
+}
 
 document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll('.nav-link');
     buttons.forEach(button => {
         button.addEventListener('click', handleClick);
     });
+    const carrusel = document.querySelector('.carrusel');
+    const slide = document.querySelectorAll('.slide');
+    const btnAnterior = document.getElementById('btnAnterior');
+    const btnSiguiente = document.getElementById('btnSiguiente');
+    btnSiguiente.addEventListener('click', handleSiguienteClick);
+    btnAnterior.addEventListener('click', handleAnteriorClick);
+    function handleSiguienteClick() {
+        imgCero = (imgCero + 1) % slide.length;
+        actualizarCarrusel();
+    }
+    function handleAnteriorClick() {
+        imgCero = (imgCero - 1 + slide.length) % slide.length;
+        actualizarCarrusel();
+    }
+    function actualizarCarrusel() {
+        carrusel.style.transform = `translateX(${-imgCero * 100}%)`;
+    }
+    function autoSlide() {
+        handleSiguienteClick();
+    }
+    setInterval(autoSlide, 3000);
 });
 
 function handleClick(event) {
     const category = event.target.getAttribute('value');
     fetchData(category);
+
 }
 
 function fetchData(category) {
@@ -26,25 +85,69 @@ function fetchData(category) {
         .then(respuesta => respuesta.json())
         .then((datos) => {
             console.log(datos);
-            mostrarDatos(container, dataList, datos);
+            mostrarDatos(container, dataList, datos, category);
         })
         .catch(error => console.log(error))
         .finally(() => console.log("proceso finalizo"));
 }
+let cont = 0;
+function convertirObjeto(item, category) {
+    cont++;
+    let objetoNuevo
+    if (category === 'characters') {
+        objetoNuevo = new Personaje(String(cont), String(item.fullName), String(item.hogwartsHouse), String(item.birthdate), String(item.interpretedBy), item.image);
+    }
+    if (category === 'books') {
+        objetoNuevo = new Libro(String(cont), String(item.title), String(item.releaseDate), String(item.pages), String(item.cover));
+    }
+    if (category === 'houses') {
+        objetoNuevo = new Casa(String(cont),String(item.house),String(item.emoji),String(item.founder),String(item.animal));
+    }
+    if (category === 'spells') {
+        objetoNuevo = new Hechizo(String(cont),String(item.spell),String(item.use));
+    }
+    return objetoNuevo;
+}
 
-function mostrarDatos(container, dataList, datos) {
+function mostrarDatos(container, dataList, datos, category) {
     datos.forEach(item => {
         const tarjeta = createCard(item);
         container.appendChild(tarjeta);
-        dataList.push(item);
-    });
-}
+        const objetoActual = convertirObjeto(item, category);
+        if(category==='characters' && dataList.length!=25){
+            dataList.push(objetoActual);
+        }
+        if(category==='books' && dataList.length!=8){
+            dataList.push(objetoActual);
+        }
+        if(category==='houses' && dataList.length!=4){
+            dataList.push(objetoActual);
+        }
+        if(category==='spells' && dataList.length!=72){
+            dataList.push(objetoActual);
+        }
+        const boton = document.getElementById('btn${objetoActual.id}')
+        boton.addEventListener('click',()=>{
+         agregarFavorito(dataList,objetoActual.id);
+      })
 
+    })
+    console.log(listaLibros);
+    console.log(listaPersonajes);
+    console.log(listaCasas);
+    console.log(listaHechizos);
+}
+function agregarFavorito(lista, id) {
+    const listaFiltrada = lista.filter((obj) => obj.id === (id));
+    favoritos.push(listaFiltrada);
+    console.log(favoritos);
+}
+let contador=0
 function createCard(item) {
     const card = document.createElement("div");
     card.className = "card text-center mb-3 ";
-
-    let imageUrl, title, description;
+    contador++;
+    let imageUrl, title, description, id;
     if (item.image) {
         imageUrl = item.image;
         title = item.fullName;
@@ -70,12 +173,13 @@ function createCard(item) {
           <p class="card-text">${description}</p>
         </div>
         <div class="card-body">
-        <a href="#" class="btn btn-dark">Añadir a favoritos</a>
+        <a href="#" id="btn${contador}" class="btn btn-dark">Añadir a favoritos</a>
         </div>
     `;
 
     return card;
 }
+
 
 function getListByCategory(category) {
     switch (category) {
@@ -91,3 +195,4 @@ function getListByCategory(category) {
             return null;
     }
 }
+
